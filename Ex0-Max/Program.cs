3fd71dc07f52e1bf3e0dfbcd29a6a2cfc5af0b8c
Maria Lipina <m.o.lip714@gmail.com>
2021-10-31 22:56:08 +0300

    find max 1

--- Ex0-Max/Program.cs
+++ Ex0-Max/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 1000;
+int a2 = 700;
+int a3 = -3;
+
+int b1 = 5;
+int b2 = 30;
+int b3 = -90;
+
+int c1 = 29;
+int c2 = 0;
+int c3 = 3333;
+
+int max1 = Max(a1, a2, a3);
+int max2 = Max(b1, b2, b3);
+int max3 = Max(c1, c2, c3);
+
+int MaxFin = Max(max1, max2, max3);
+
+Console.WriteLine(MaxFin);
