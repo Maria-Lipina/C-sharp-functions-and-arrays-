e19b69e6779fe0e4b74498827f1e8d95ed3c7051
Maria Lipina <m.o.lip714@gmail.com>
2021-11-03 17:36:25 +0300

    -console. + user-friendly

--- S-affected-quadratic/Program.cs
+++ S-affected-quadratic/Program.cs
@@ -1,4 +1,6 @@
-﻿//Квадратное уравнение общего вида: ax^2+bx+c=0
+﻿using static System.Console;
+
+//Квадратное уравнение общего вида: ax^2+bx+c=0
 //Методы:
 
 double OneSolution (double a, double b, double c)
@@ -12,45 +14,47 @@ double TwoSolutions (double a, double b, double c)
 {
     double D = b*b - a*c*4;
     double x1 = (-b + Math.Sqrt(D)) / 2*a;
-    Console.WriteLine (x1);
+    WriteLine (x1);
     double x2 = (-b - Math.Sqrt(D)) / 2*a;
     return x2;
 }
 
 // Собственно программа
 
-Console.WriteLine("Для решения квадратного уравнения введите коэффициенты");
+WriteLine("Для решения уравнения ax^2+bx+c=0 введите коэффициенты");
 
-Console.Write("a = ");
-double a1 = double.Parse(Console.ReadLine());
+Write("a = ");
+double a1 = double.Parse(ReadLine());
 
 if (a1 == 0) 
 {
-    Console.WriteLine ("Недопустимое значение a");
+    WriteLine ("Недопустимое значение a");
     Environment.Exit(1);
 // как здесь прописать "компьютер, верни пользователя обратно к введению а1"?
 }
 
-Console.Write("b = ");
-double b1 = double.Parse(Console.ReadLine());
+Write("b = ");
+double b1 = double.Parse(ReadLine());
 
-Console.Write("c = ");
-double c1 = double.Parse(Console.ReadLine());
+Write("c = ");
+double c1 = double.Parse(ReadLine());
 
 double D1 = b1*b1 - a1*c1*4;
 
 if (D1 < 0)
 {
-    Console.WriteLine ("Нет корней");
+    WriteLine ("Нет корней");
 }
 if (D1 == 0)
 {
+    Write ("Корни уравнения: ");
     double result = OneSolution(a1, b1, c1);
-    Console.WriteLine (result);
+    WriteLine (result);
 }
 if (D1 > 0)
 {
+    Write ("Корни уравнения: ");
     double result = TwoSolutions (a1, b1, c1);
-    Console.WriteLine (result);
+    WriteLine (result);
 }
 
