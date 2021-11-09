efc7c6efab3bf097c4408b4ec91bbcdaabf600b2
Maria Lipina <m.o.lip714@gmail.com>
2021-11-09 23:36:10 +0300

    Lecture 02.11.2021 part 2

---
+++ L-Sorting/Program.cs
@@ -0,0 +1,32 @@
+﻿void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+Console.WriteLine();
+}
+
+void Sorting(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++) // сделал код на одну строку меньше (сравни с PrintArray)
+    {
+        int minPosition = i; //позиция рабочего элемента, для которой мы будем производить действия
+
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if (array[j] > array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i]; // чтобы поменять положение minPosition с той позицией, которую нашли при выполнении строчек выше
+        array[i] = array[minPosition]; //чтобы поменять переменные местами. Положить в позицию i тот элемент, который нашли в строчках выше, а значит который удовлетворяет условию сортировки и должен отображаться в выходном массиве на данном месте
+        array[minPosition] = temporary;
+    } 
+}
+
+int[] arr = { 3, 6, 8, 0, 2, 4, 10, 3, 8, 7 };
+PrintArray(arr);
+Sorting(arr);
+PrintArray(arr); 
+
