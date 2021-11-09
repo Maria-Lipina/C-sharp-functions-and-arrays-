ef90005311d8ddd42c9813103213641fd09f615e
Maria Lipina <m.o.lip714@gmail.com>
2021-11-09 23:34:30 +0300

    Lecture 02.11.2021

--- L-types-of-method/Program.cs
+++ L-types-of-method/Program.cs
@@ -66,10 +66,10 @@ WriteLine(res);
 
 for (int i = 2; i <= 10; i++)
 {
-for (int j = 2; j <= 10; j++)
-{
+    for (int j = 2; j <= 10; j++)
+    {
     WriteLine($"{i} * {j} = {i*j}");
-}
+    }
 WriteLine();
 }
 
