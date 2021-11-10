4135bc07553632e0577c59fea349bdd67b2f9105
Maria Lipina <m.o.lip714@gmail.com>
2021-11-10 21:14:33 +0300

    task 3

---
+++ S1-Lifecoding/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+WriteLine();
+WriteLine("3. По заданному номеру дня недели вывести его название");
+
+int day = Convert.ToInt32(ReadLine());
+
+string name = day switch
+{
+1 => "понедельник",
+2 => "вторник",
+3 => "среда",
+4 => "четверг",
+5 => "пятница",
+6 => "суббота",
+7 => "воскресенье",
+_ => "нет дня недели с таким номером"
+};
+WriteLine(name);
