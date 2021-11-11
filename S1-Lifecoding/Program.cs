9a1acd8ebe7ca9d1addf31ba2d93786fd39fe435
Maria Lipina <m.o.lip714@gmail.com>
2021-11-11 19:44:39 +0300

    massive from -n to N

--- S1-Lifecoding/Program.cs
+++ S1-Lifecoding/Program.cs
@@ -1,2 +1,23 @@
 ﻿using static System.Console;
 using static System.Convert;
+
+WriteLine();
+WriteLine("7. Показать числа от -N до N");
+void PrintArray(int[] collect)
+{
+    for (int pos = 0; pos < collect.Length; pos ++) Console.Write($"{collect[pos]} ");
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+int [] FillArray(int N)
+{
+int[] collect = new int[N+N+1];
+for (int pos = 0; pos < collect.Length; pos++) 
+{
+    collect[pos] = -N;
+N--; // тот самый момент, когда сначала написал код и только потом понял, почему он работает как надо.
+}
+return collect;
+}
+PrintArray(FillArray(7));
