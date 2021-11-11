079228c5d8b986196edbb8d5037a683f988ff0cf
Maria Lipina <m.o.lip714@gmail.com>
2021-11-11 19:46:17 +0300

    task 7

--- S1-Lifecoding/Program.cs
+++ S1-Lifecoding/Program.cs
@@ -87,12 +87,27 @@ else {Console.WriteLine("нечетное");}
 WriteLine();
 WriteLine("7. Показать числа от -N до N");
 
-// int N = 5;
+WriteLine();
+WriteLine("7. Показать числа от -N до N");
+void PrintArray(int[] collect)
+{
+    for (int pos = 0; pos < collect.Length; pos ++) Console.Write($"{collect[pos]} ");
+    Console.WriteLine();
+    Console.WriteLine();
+}
 
-// for (int i = -N; i<= N; i++)
-// {
-//     Console.WriteLine(i);
-// }
+int [] FillArray(int N)
+{
+int[] collect = new int[N+N+1];
+for (int pos = 0; pos < collect.Length; pos++) 
+{
+    collect[pos] = -N;
+N--; // тот самый момент, когда сначала написал код и только потом понял, почему он работает как надо.
+}
+return collect;
+}
+PrintArray(FillArray(7));
+
 WriteLine();
 WriteLine("8. Показать четные числа от 1 до N");
 
