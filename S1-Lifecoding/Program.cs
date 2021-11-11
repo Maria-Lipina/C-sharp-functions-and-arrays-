10195bbae57a301fb1463d3f9049fd4f3aca7283
Maria Lipina <m.o.lip714@gmail.com>
2021-11-11 18:43:50 +0300

    clear

--- S1-Lifecoding/Program.cs
+++ S1-Lifecoding/Program.cs
@@ -1,8 +1,2 @@
 ﻿using static System.Console;
-
-double sin(double a)
-{
-    return Math.Asin(a); //тот самый момент когда код работает, но как? Непонятно
-}
-double y1 = sin(1);
-Console.WriteLine(y1);
+using static System.Convert;
